fefc2a3f0684259d9399dd516d87c2c1b3e3ccdf
chri3g91 <chri3g91@edu.eal.dk>
2014-09-11 11:04:46 +0200

    test class
    ccs

---
+++ AuctionHouseTCSF/OtherClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseTCSF
+{
+    class OtherClass
+    {
+        public string hi = "HI";
+    }
+}
