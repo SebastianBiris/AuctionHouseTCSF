b00a69c985fab5b2d8523075ad80b26b27b8f45e
kamenova92 <tkamenova92@gmail.com>
2014-09-15 12:43:01 +0200

    Item class

--- AuctionHouseServer/Item.cs
+++ AuctionHouseServer/Item.cs
@@ -8,5 +8,36 @@ namespace AuctionHouseServer
 {
     class Item
     {
+        private string name;
+        private long startPrice;
+        private long currPrice;
+
+        //constructor
+        public Item(string name, long startPrice, long currPrice)
+        {
+            this.name = name;
+            this.startPrice = startPrice;
+            this.currPrice = currPrice;
+        }
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public long StartPrice
+        {
+            get { return startPrice; }
+            set { startPrice = value; }
+        }
+
+        public long CurrPrice
+        {
+            get { return currPrice; }
+            set { currPrice = value; }
+        }
+        #endregion
     }
 }
